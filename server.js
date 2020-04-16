68497134c7262b9948dbf6751d2e394d3d4cab7e
Tushar Mittal <tushar.mittal@nagarro.com>
2020-04-17 02:26:02 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const{ db } = require('../NodeFinal/db')
+const todoRoute = require('./routes/todos')
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/',express.static(__dirname+'/public'));
+
+app.use('/todos',todoRoute)
+
+
+db.sync()
+  .then(()=>{
+    app.listen(2200)
+  })
+  .catch((err)=>{
+    console.error(err)
+  })
